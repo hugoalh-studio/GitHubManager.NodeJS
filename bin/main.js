498461a992a2ee8393f9e1ef46f4884d52348f66
hugoalh <32359235+hugoalh@users.noreply.github.com>
2021-06-30 17:50:26 +0800

    20210630B

--- bin/main.js
+++ bin/main.js
@@ -2,22 +2,19 @@
 const commandLineParser = require("@hugoalh/command-line-parser"),
 	header = require("../lib/internal/header.js"),
 	internalConsole = require("../lib/internal/console.js"),
-	internalFlag = require("../lib/internal/flag.js"),
 	languageService = require("../lib/language/main.js"),
-	sessionCode = require("../lib/internal/session-code.js");
+	sessionFlag = require("../lib/internal/session-flag.js");
 let commandLine = commandLineParser(process.argv.slice(2));
 if (commandLine.flag.includes("silent") === false) {
 	console.log(header);
 };
-if (commandLine.fault.length > 0) {
-	commandLine.fault.forEach((element) => {
-		internalConsole.warning(`${languageService.warningUnknownInput_1}${element}${languageService.warningUnknownInput_2}`);
-	});
-};
-commandLine.flag.push(`command_${sessionCode}`);
+commandLine.fault.forEach((element) => {
+	internalConsole.warning(languageService.warningUnknownInput, { input: element });
+});
+commandLine.flag.push(sessionFlag.panel);
 if (commandLine.action.length === 0 && commandLine.fault.length === 0 && commandLine.flag.length === 1 && Object.keys(commandLine.option).length === 0) {
-	commandLine.flag.push(`${internalFlag.wizard}`);
-	require("../lib/wizard/main.js")(commandLine);
+	commandLine.flag.push(sessionFlag.wizard);
+	require("../lib/wizard.js")(commandLine);
 } else {
-	require("../lib/direct/main.js")(commandLine);
+	require("../lib/direct.js")(commandLine);
 };

6329fb4dc51c5a2597ff309553b6af109b1baf05
hugoalh <32359235+hugoalh@users.noreply.github.com>
2021-07-03 19:10:10 +0800

    20210703A

--- bin/main.js
+++ bin/main.js
@@ -3,16 +3,15 @@ const commandLineParser = require("@hugoalh/command-line-parser"),
 	header = require("../lib/internal/header.js"),
 	internalConsole = require("../lib/internal/console.js"),
 	internalFlag = require("../lib/internal/flag.js"),
-	languageService = require("../lib/language/main.js");
+	ghmLanguage = require("../lib/language/main.js");
 let commandLine = commandLineParser(process.argv.slice(2));
 if (commandLine.flag.includes("silent") === false) {
 	console.log(header);
 };
 commandLine.fault.forEach((element) => {
-	internalConsole.warning(languageService.warningUnknownInput, { input: element });
+	internalConsole.warning(ghmLanguage.warningUnknownInput, { input: element });
 });
-commandLine.flag.push(internalFlag.panel);
-if (commandLine.action.length === 0 && commandLine.fault.length === 0 && commandLine.flag.length === 1 && Object.keys(commandLine.option).length === 0) {
+if (commandLine.action.length === 0 && commandLine.fault.length === 0 && commandLine.flag.length === 0 && Object.keys(commandLine.option).length === 0) {
 	commandLine.flag.push(internalFlag.wizard);
 	require("../lib/wizard.js")(commandLine);
 } else {

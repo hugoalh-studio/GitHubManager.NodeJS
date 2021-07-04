ef1d498ad95f312a3ddf93b64fb828857898a90d
hugoalh <32359235+hugoalh@users.noreply.github.com>
2021-07-04 19:10:13 +0800

    20210704A

--- bin/main.js
+++ bin/main.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 const commandLineParser = require("@hugoalh/command-line-parser"),
-	header = require("../lib/internal/header.js"),
-	internalConsole = require("../lib/internal/console.js"),
-	internalFlag = require("../lib/internal/flag.js"),
-	ghmLanguage = require("../lib/language/main.js");
+	ghmConsole = require("../lib/internal/console.js"),
+	ghmFlag = require("../lib/internal/flag.js"),
+	ghmLanguage = require("../lib/language/main.js"),
+	header = require("../lib/internal/header.js");
 let commandLine = commandLineParser(process.argv.slice(2));
 if (commandLine.flag.includes("silent") === false) {
 	console.log(header);
 };
 commandLine.fault.forEach((element) => {
-	internalConsole.warning(ghmLanguage.warningUnknownInput, { input: element });
+	ghmConsole.warning(ghmLanguage.warningUnknownInput, { input: element });
 });
 if (commandLine.action.length === 0 && commandLine.fault.length === 0 && commandLine.flag.length === 0 && Object.keys(commandLine.option).length === 0) {
-	commandLine.flag.push(internalFlag.wizard);
+	commandLine.flag.push(ghmFlag.wizard);
 	require("../lib/wizard.js")(commandLine);
 } else {
 	require("../lib/direct.js")(commandLine);
